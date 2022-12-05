c65f2848bee66177a5a36c720a4ba6e2d7f7ea9a
Anatoliy Melnyk <toliamel@gmail.com>
2022-12-05 19:45:35 +0200

    some fixes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Установка после загрузки
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded");
 });
